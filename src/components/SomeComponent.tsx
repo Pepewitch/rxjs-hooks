0ce823cf32414d2bf1e3255a51abaf2447848e3f
pepe_witch <skpepe134@gmail.com>
2019-02-23 18:40:16 +0700

    test screen size hooks

--- src/components/SomeComponent.tsx
+++ src/components/SomeComponent.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { ReactComponent as Hamburger } from "../assets/bars-solid.svg";
 import styles from "./SomeComponent.module.scss";
+import { useScreenSize, ScreenSize } from "../services/ScreenService";
 
 export default () => {
+  const size = useScreenSize();
   return (
     <div>
       <Hamburger className={styles.icon} />
-      Some Component
+      {size === ScreenSize.desktop
+        ? "desktop"
+        : size === ScreenSize.tablet
+        ? "tablet"
+        : "mobile"}
     </div>
   );
 };
